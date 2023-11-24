bcc6d821e5721179bb8040de9784a1e66be38172
Brian Davis <thebrianjdavis@gmail.com>
2023-11-24 17:52:27 -0500

    RoBERTa code moved to separate different models

--- Final Project/RoBERTa/main_R.py
+++ Final Project/RoBERTa/main_R.py
@@ -1,24 +1,24 @@
 # Import necessary modules
 import pandas as pd
-import torch.backends.mps
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report
 from preprocessing import preprocess_data
-from feature_extraction import extract_features
+from feature_extraction_R import extract_features
 import logging
 
 # Set up the logger
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Load the data
-df = pd.read_csv('Datasets/mini_lyrics_raw.csv', on_bad_lines='skip')
+df = pd.read_csv('../Datasets/mini_lyrics_raw.csv', on_bad_lines='skip')
 
 # Preprocess data
 preprocessed_df = preprocess_data(df)
 
 # Splitting the dataset into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(preprocessed_df['lyrics'], preprocessed_df['tag'], test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    preprocessed_df['lyrics'], preprocessed_df['tag'], test_size=0.2, random_state=42)
 
 # Extract features
 logging.info("Starting feature extraction...")
